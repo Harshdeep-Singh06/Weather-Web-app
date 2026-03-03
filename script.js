80c81fa1b26ad44cb9a69a654e26cd74aa60ef99
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-03-03 10:20:35 +0530

    Convert data to json

--- script.js
+++ script.js
@@ -7,5 +7,7 @@ searchBtn.addEventListener('click', async function(e){
     const apiKey = "50f14407c3c54e8fc907804711c275cf";
     const Url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=50f14407c3c54e8fc907804711c275cf&units=metric`
     const response = await fetch(Url);
-    console.log(response);
+    const data  = await response.json();
+    console.log(data);
+    
 })
