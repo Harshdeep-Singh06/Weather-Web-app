a940b4ff4d87af13253f19ce0df008b69bc5accb
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-03-03 10:16:53 +0530

    Add async fetch for weather Api

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const searchBtn = document.querySelector("button");
 const searchInput = document.querySelector("input");
 
-searchBtn.addEventListener('click', function(e){
+searchBtn.addEventListener('click', async function(e){
     const city = searchInput.value;
     console.log(city);
     const apiKey = "50f14407c3c54e8fc907804711c275cf";
     const Url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=50f14407c3c54e8fc907804711c275cf&units=metric`
-    console.log(Url);
+    const response = await fetch(Url);
+    console.log(response);
 })
